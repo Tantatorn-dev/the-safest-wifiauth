ec8464d8482f3bb29b3acb2ae3965f93c7fda855
Timefear007 <timefear007@gmail.com>
2019-04-20 15:25:12 +0700

    add axios

--- src/components/captcha/captcha1/Captcha1.vue
+++ src/components/captcha/captcha1/Captcha1.vue
@@ -2,17 +2,27 @@
   <v-layout>
     <v-flex xs12 sm6 offset-sm3>
       <v-card>
-
+        
         <v-card-title primary-title>
-          <div>
-            <h3 class="headline mb-0">Captcha</h3>
-          </div>
+            <v-card class="captcha-head">
+              <h1>Select all images with Pepsi logo</h1>
+            </v-card>
         </v-card-title>
 
+        <v-card-media>
+           <v-container align-center justify-center fluid grid-list-xl>
+              <v-layout row wrap>
+              <v-flex v-for="i in num" :key="i" xs4>
+                <img  :src="`https://randomuser.me/api/portraits/men/${i + 20}.jpg`" class="image" alt="lorem" width="100%" height="100%">
+              </v-flex>
+              </v-layout>
+            </v-container>
+        </v-card-media>
+
         <v-card-actions>
-          <v-btn flat color="orange">Share</v-btn>
-          <v-btn flat color="orange">Explore</v-btn>
+          <v-btn flat color="primary">Verify</v-btn>
         </v-card-actions>
+
       </v-card>
     </v-flex>
   </v-layout>
@@ -20,7 +30,21 @@
 
 <script>
 export default {
-    name: "Captcha1"
+    name: "Captcha1",
+    data(){
+      return{
+      }
+    }
 }
 </script>
 
+<style scoped>
+.captcha-head{
+  width:100%;
+  background: #4990e2;
+  color:white;
+}
+.image:hover{
+  opacity: 0.5;
+}
+</style>
