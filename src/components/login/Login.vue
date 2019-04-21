01a69b017d2c6f98d1eb393029df3432321637f5
Phumphathai Chansriwong <ouoam2555@gmail.com>
2019-04-21 09:32:10 +0700

    make ui better

--- src/components/login/Login.vue
+++ src/components/login/Login.vue
@@ -1,67 +1,47 @@
 <template>
-  <div id="app">
-    <v-app id="inspire">
-      <v-content>
-        <v-container fluid fill-height>
-          <v-layout justify-center>
-            <v-flex xs12 sm8 md4>
-              <v-card class="elevation-12">
-                <v-toolbar dark color="primary">
-                  <v-toolbar-title>Login form</v-toolbar-title>
-                  <v-spacer></v-spacer>
-                  <v-tooltip bottom>
-                    <v-btn
-                      icon
-                      large
-                      :href="source"
-                      target="_blank"
-                      slot="activator"
-                    >
-                      <v-icon large>code</v-icon>
-                    </v-btn>
-                    <span>Source</span>
-                  </v-tooltip>
-                </v-toolbar>
-                <v-card-text>
-                  <v-form>
-                    <v-text-field
-                      prepend-icon="person"
-                      name="login"
-                      label="Login"
-                      type="text"
-                    ></v-text-field>
-                    <v-text-field
-                      prepend-icon="lock"
-                      name="password"
-                      label="Password"
-                      id="password"
-                      type="password"
-                    ></v-text-field>
-                  </v-form>
-                </v-card-text>
-                <v-card-actions>
-                  <v-spacer></v-spacer>
-                  <v-layout row justify-center>
-                    <v-dialog
-                      v-model="dialog"
-                      fullscreen
-                      hide-overlay
-                      transition="dialog-bottom-transition"
-                    >
-                      <template v-slot:activator="{ on }">
-                        <v-btn color="primary" v-on="on">Login</v-btn>
-                      </template>
-                      <Captcha1></Captcha1>
-                    </v-dialog>
-                  </v-layout>
-                </v-card-actions>
-              </v-card>
-            </v-flex>
-          </v-layout>
-        </v-container>
-      </v-content>
-    </v-app>
-  </div>
+  <v-container fluid fill-height>
+    <v-layout justify-center>
+      <v-flex xs12 sm12 md5>
+        <v-card class="elevation-12">
+          <v-toolbar dark color="primary">
+            <v-toolbar-title
+              >Please log on to use the internet hotspot
+              service</v-toolbar-title
+            >
+          </v-toolbar>
+          <v-card-text>
+            <v-form>
+              <v-text-field
+                prepend-icon="person"
+                name="login"
+                label="Login"
+                type="text"
+              ></v-text-field>
+              <v-text-field
+                prepend-icon="lock"
+                name="password"
+                label="Password"
+                id="password"
+                type="password"
+              ></v-text-field>
+            </v-form>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="primary" @click="dialog = true">Login</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+    </v-layout>
+    <v-dialog
+      v-model="dialog"
+      fullscreen
+      hide-overlay
+      transition="dialog-bottom-transition"
+    >
+      <Captcha1></Captcha1>
+    </v-dialog>
+  </v-container>
 </template>
 
 <script>
